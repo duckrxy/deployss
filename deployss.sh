1ff21b1ac478ddacb55e8e87e57c5d23da970b96
Ubuntu <azureuser@vpndev.vpndev.d6.internal.cloudapp.net>
2015-06-09 15:06:16 +0000

    auto fill password

--- deployss.sh
+++ deployss.sh
@@ -3,4 +3,10 @@
 sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install shadowsocks
-
+wget https://raw.githubusercontent.com/duckrxy/deployss/master/shadowsocks.json
+echo "input shadowsocks password:"
+read sspwd
+echo $sspwd
+sed -i "s/\"password\":\"[a-zA-Z1-9~!@#$%^&*()_+-=]*\"/\"password\":\"${sspwd}\"/" shadowsocks.json
+sudo cp -f shadowsocks.json /etc/shadowsocks.json
+sudo ssserver -c /etc/shadowsocks.json -d start
